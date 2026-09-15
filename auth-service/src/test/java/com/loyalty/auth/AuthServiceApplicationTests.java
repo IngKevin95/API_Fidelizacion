80cee6c7e3b919c97b0c885aada9fd70eb909a5a
IngKevin95 <kevin.f.o@hotmail.com>
2026-09-15 04:17:41 -0500

    feat: inicializa auth-service con dependencias, config y test de contexto

---
+++ auth-service/src/test/java/com/loyalty/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.loyalty.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = "eureka.client.enabled=false")
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
