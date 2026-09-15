3f662c16411a76c08578b0e70830a465cfcd907c
IngKevin95 <kevin.f.o@hotmail.com>
2026-09-15 03:34:51 -0500

    feat: agrega AccountClient que consulta account-service via Eureka reenviando el JWT

---
+++ transfer-service/src/main/java/com/loyalty/transfer/client/AccountClient.java
@@ -0,0 +1,38 @@
+package com.loyalty.transfer.client;
+
+import com.loyalty.transfer.dto.AccountView;
+import com.loyalty.transfer.exception.AccountNotFoundException;
+import com.loyalty.transfer.exception.TransferAccessDeniedException;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class AccountClient {
+
+    private final RestClient restClient;
+
+    public AccountClient(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.baseUrl("http://account-service").build();
+    }
+
+    public AccountClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public AccountView fetchAccount(String accountId, String bearerToken) {
+        return restClient.get()
+                .uri("/accounts/{id}", accountId)
+                .header("Authorization", "Bearer " + bearerToken)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                    if (response.getStatusCode().value() == 404) {
+                        throw new AccountNotFoundException("Cuenta no encontrada: " + accountId);
+                    }
+                    if (response.getStatusCode().value() == 403) {
+                        throw new TransferAccessDeniedException("No autorizado para acceder a la cuenta " + accountId);
+                    }
+                })
+                .body(AccountView.class);
+    }
+}
